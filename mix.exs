d94c5411185f27c885b2887f50185d0e8e981714
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-11-28 11:03:16 +0800

    Add gun for managing connections

--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule CryptoDashboard.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:gun, "~> 1.3"},
+      {:cowlib, "~> 2.11", override: true},
     ]
   end
 
