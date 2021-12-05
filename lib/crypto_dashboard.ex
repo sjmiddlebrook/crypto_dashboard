a743c47f45d9b70648f1fb3a226f7e21e07c6ff1
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-12-05 21:24:09 +0800

    Add historical trades for clients

--- lib/crypto_dashboard.ex
+++ lib/crypto_dashboard.ex
@@ -1,4 +1,7 @@
 defmodule CryptoDashboard do
   defdelegate subscribe_to_trades(product), to: CryptoDashboard.Exchanges, as: :subscribe
   defdelegate unsubscribe_from_trades(product), to: CryptoDashboard.Exchanges, as: :unsubscribe
+
+  defdelegate get_last_trade(product), to: CryptoDashboard.Historical
+  defdelegate get_last_trades(products), to: CryptoDashboard.Historical
 end
--- lib/crypto_dashboard/exchanges.ex
+++ lib/crypto_dashboard/exchanges.ex
@@ -1,6 +1,21 @@
 defmodule CryptoDashboard.Exchanges do
   alias CryptoDashboard.{Product, Trade}
 
+  @clients [
+    CryptoDashboard.Exchanges.CoinbaseClient,
+    CryptoDashboard.Exchanges.BitstampClient
+  ]
+
+  @available_products (for client <- @clients, pair <- client.available_currency_pairs() do
+    Product.new(client.exchange_name(), pair)
+  end)
+
+  @spec clients() :: [module()]
+  def clients, do: @clients
+
+  @spec available_products() :: [Product.t()]
+  def available_products(), do: @available_products
+
   @spec subscribe(Product.t()) :: :ok | {:error, term()}
   def subscribe(product) do
     Phoenix.PubSub.subscribe(CryptoDashboard.PubSub, topic(product))
